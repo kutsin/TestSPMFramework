3be3e90e60b5039539c1b4e9a4da4df66a738b06
mr.kutsin <mr.kutsin@gmail.com>
2024-10-17 22:05:31 -0500

    update

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SravniLib",
-            url: "https://www.dropbox.com/scl/fi/qsre0bg91xv3ti26hihvl/SravniLib.xcframework.zip?rlkey=et5dv0o5nj86hygr0wdp9jltn&st=9iuaanwi&dl=1",
-            checksum: "c5fc568233e45aec722d51ab19a5ffcb399047e699ab0559638f902cb787b71a"
+            url: "https://www.dropbox.com/scl/fi/a3nwp019qxxah82bud08a/SravniLib.xcframework.zip?rlkey=suzvyz2f2kt3eeusyt7kmwwvv&st=93mxtzb0&dl=1",
+            checksum: "b37c9b66f9996ad12c94d69ea33a0e28832d22b95d26d49f3148e81d952e8167"
         )
     ]
 )
